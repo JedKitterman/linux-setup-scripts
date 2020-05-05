e5f32b2d1fcdff0039fc7e4b6952bf0360556235
Andrew <andrewpynchbusiness@gmail.com>
2020-05-04 18:11:46 -0700

    Adding the shit yo

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+
+# Other programs
+sudo snap install --classic code
+sudo apt install git
+sudo apt-get install chromium-browser
+sudo apt install sl
+
+echo "To setup git with your creds sign in and... git config --global credential.helper store"
+
+cd ~/Github/linux-setup-scripts
+cp settings.json ~/.config/Code/User/settings.json
+
+
